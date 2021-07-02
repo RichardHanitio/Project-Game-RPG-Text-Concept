0a47f66586bd11fef076d051eeaafea552c1c568
RichardH <03082200014@student.uph.edu>
2021-07-02 15:12:26 +0700

    added Ogre, Yeti, adn Orochi

---
+++ src/Ogre.java
@@ -0,0 +1,21 @@
+public class Ogre extends Enemy{
+    @Override
+    public void attack() {
+        
+    }
+
+    @Override
+    public void skill1() {
+
+    }
+
+    @Override
+    public void skill2() {
+
+    }
+
+    @Override
+    public void skill3() {
+
+    }
+}
---
+++ src/Yeti.java
@@ -0,0 +1,21 @@
+public class Yeti extends Enemy{
+    @Override
+    public void attack() {
+
+    }
+
+    @Override
+    public void skill1() {
+
+    }
+
+    @Override
+    public void skill2() {
+
+    }
+
+    @Override
+    public void skill3() {
+
+    }
+}
