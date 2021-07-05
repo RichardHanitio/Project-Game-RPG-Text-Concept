2c9395929d51f738dae92dd9282d41731e1f4e6f
Calvinwen10 <calvinwendi.002cw@gmail.com>
2021-07-05 11:23:25 +0700

    Update Backpack.java

--- src/mainclasses/Backpack.java
+++ src/mainclasses/Backpack.java
@@ -1,4 +1,44 @@
 package mainclasses;
+
 public class Backpack {
-    
+    int healthPotion = 5;
+    int manaPotion = 3;
+    String Weapon;
+
+
+    public Backpack() {
+    }
+
+
+    public int getHealthPotion() {
+        return this.healthPotion;
+    }
+
+    public void setHealthPotion(int healthPotion) {
+        this.healthPotion = healthPotion;
+    }
+
+    public int getManaPotion() {
+        return this.manaPotion;
+    }
+
+    public void setManaPotion(int manaPotion) {
+        this.manaPotion = manaPotion;
+    }
+
+    public String getWeapon() {
+        return this.Weapon;
+    }
+
+    public void setWeapon(String Weapon) {
+        this.Weapon = Weapon;
+    }
+
+    public void usePotionHealth() {
+
+    }
+
+    public void usePotionMana() {
+        
+    }
 }
