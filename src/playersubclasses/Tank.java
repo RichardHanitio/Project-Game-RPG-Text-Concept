b9627fd7781be0d298545c8b260346682e373ca1
Calvinwen10 <calvinwendi.002cw@gmail.com>
2021-07-22 21:49:08 +0700

    Update Tank.java

--- src/playersubclasses/Tank.java
+++ src/playersubclasses/Tank.java
@@ -5,6 +5,28 @@ import mainclasses.Player;
 import mainclasses.Weapon;
 import mainclasses.Battle;
 
-public class Tank {
-    
+/*
+Nama : Calvin Wendy
+NIM : 03082200010
+*/
+
+public class Tank extends Player{
+    //properties
+    private Weapon weapon;
+
+    //constructor
+    public Tank(String name){
+        //Nama berasal dari player
+        //health awal   : 1000
+        //str awal      : 5
+        //mana awal     : 0
+        super(name,1000,0,5,0);
+        super.setClassID(1); //weapon tank = weapon fighter
+        this.weapon = new Weapon(getClassID());
+        super.setWeapon(weapon); 
+    }
+
+    public Tank(String name, int health, int mana, int attSTR, int attINT){
+        
+    }    
 }
