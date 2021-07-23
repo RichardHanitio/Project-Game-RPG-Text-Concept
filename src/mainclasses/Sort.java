cd7de0509421e2b88a29313084d52f42cf8f5b6c
Silverlake86 <78220117+Silverlake86@users.noreply.github.com>
2021-07-23 20:13:22 +0700

    Update Sort.java

--- src/mainclasses/Sort.java
+++ src/mainclasses/Sort.java
@@ -1,16 +1,16 @@
 package mainclasses;
 
 public class Sort {
-    
-    //Sort by Enemy Health from the biggest to the smallest with Selection Sort
-    //Nama  : Ricky
-    //NIM   : 03082200012
-    public Enemy[] selectionSort(Enemy[] enemy){
+
+    // Sort by Enemy Health from the biggest to the smallest with Selection Sort
+    // Nama : Ricky
+    // NIM : 03082200012
+    public Enemy[] selectionSort(Enemy[] enemy) {
         Enemy sortedEnemyList[] = enemy;
 
-        for(int i = 0 ; i < sortedEnemyList.length ; i++){
-            for (int j = i + 1 ; j <= sortedEnemyList.length-1 ; j++){
-                if(sortedEnemyList[i].getHealth() < sortedEnemyList[j].getHealth()){
+        for (int i = 0; i < sortedEnemyList.length; i++) {
+            for (int j = i + 1; j <= sortedEnemyList.length - 1; j++) {
+                if (sortedEnemyList[i].getHealth() < sortedEnemyList[j].getHealth()) {
                     Enemy tampungan = sortedEnemyList[i];
                     sortedEnemyList[i] = sortedEnemyList[j];
                     sortedEnemyList[j] = tampungan;
@@ -20,14 +20,15 @@ public class Sort {
         return sortedEnemyList;
     }
 
-    //Sort by Player Health from the biggest to the smallest with Selection Sort (Method Overloading)
-    //Nama  : Ricky
-    //NIM   : 03082200012
-    public Player[] selectionSort(Player[] player){
+    // Sort by Player Health from the biggest to the smallest with Selection Sort
+    // (Method Overloading)
+    // Nama : Ricky
+    // NIM : 03082200012
+    public Player[] selectionSort(Player[] player) {
         Player sortedPlayerList[] = player;
-        for(int i = 0 ; i < sortedPlayerList.length ; i++){
-            for (int j = i + 1 ; j <= sortedPlayerList.length-1 ; j++){
-                if(sortedPlayerList[i].getClassOriginalHealth() < sortedPlayerList[j].getClassOriginalHealth()){
+        for (int i = 0; i < sortedPlayerList.length; i++) {
+            for (int j = i + 1; j <= sortedPlayerList.length - 1; j++) {
+                if (sortedPlayerList[i].getClassOriginalHealth() < sortedPlayerList[j].getClassOriginalHealth()) {
                     Player tampungan = sortedPlayerList[i];
                     sortedPlayerList[i] = sortedPlayerList[j];
                     sortedPlayerList[j] = tampungan;
@@ -37,36 +38,33 @@ public class Sort {
         return sortedPlayerList;
     }
 
-
-    //Sort Player Health from largest to smallest with bubble sort
+    // Sort Player Health from largest to smallest with bubble sort
     /*
-    Nama : Richard Hanitio
-    NIM : 03082200014
-    */
+     * Nama : Richard Hanitio NIM : 03082200014
+     */
 
-    public Player[] bubbleSort(Player[] listPlayer){
+    public Player[] bubbleSort(Player[] listPlayer) {
         Player[] sortedPlayerList = listPlayer;
-        for (int k=0; k<sortedPlayerList.length-1; k++){
-            for(int i=0; i<sortedPlayerList.length-1; i++){
-                if(sortedPlayerList[i].getClassOriginalHealth() < sortedPlayerList[i+1].getClassOriginalHealth()){
+        for (int k = 0; k < sortedPlayerList.length - 1; k++) {
+            for (int i = 0; i < sortedPlayerList.length - 1; i++) {
+                if (sortedPlayerList[i].getClassOriginalHealth() < sortedPlayerList[i + 1].getClassOriginalHealth()) {
                     Player temp = sortedPlayerList[i];
-                    sortedPlayerList[i] = sortedPlayerList[i+1];
-                    sortedPlayerList[i+1] = temp;
+                    sortedPlayerList[i] = sortedPlayerList[i + 1];
+                    sortedPlayerList[i + 1] = temp;
                 }
             }
         }
         return sortedPlayerList;
     }
 
-    //Sort Enemy Health from smallest to largest with shell sort
+    // Sort Enemy Health from smallest to largest with shell sort
     /*
-    Nama : Calvin Wendy
-    NIM : 03082200010
-    */
+     * Nama : Calvin Wendy NIM : 03082200010
+     */
     public Enemy[] shellSort(Enemy[] enemy) {
         int n = enemy.length;
         Enemy[] musuh = enemy;
-    
+
         for (int gap = n / 2; gap > 0; gap /= 2) {
             for (int i = gap; i < n; i++) {
                 Enemy key = musuh[i];
@@ -79,5 +77,41 @@ public class Sort {
             }
         }
         return musuh;
+    }
+
+    // Sort Enemy Health from the lowest to the highest amount using insertion sort
+    // Nama : Setiawan Junior
+    // NIM : 03082180037
+    public Enemy[] quickSort(Enemy[] enemy) {
+        Enemy sortedEnemyList[] = enemy;
+        for (int i = 1; i < sortedEnemyList.length; i++) {
+            int j = i;
+            while ((j > 0) && (sortedEnemyList[j - 1].getHealth() > sortedEnemyList[j].getHealth())) {
+                Enemy tampungan = sortedEnemyList[j];
+                sortedEnemyList[j] = sortedEnemyList[j - 1];
+                sortedEnemyList[j - 1] = tampungan;
+                j = j - 1;
+            }
+        }
+        return sortedEnemyList;
+    }
+
+    // Sort Character class strength (STR) from the lowest to the highest amount
+    // using insertion sort
+    // Nama : Setiawan Junior
+    // NIM : 03082180037
+    public Player[] quickSort(Player[] player) {
+        Player sortedPlayerList[] = player;
+        for (int i = 1; i < sortedPlayerList.length; i++) {
+            int j = i;
+            while ((j > 0)
+                    && (sortedPlayerList[j - 1].getClassOriginalSTR() > sortedPlayerList[j].getClassOriginalSTR())) {
+                Player tampungan = sortedPlayerList[j];
+                sortedPlayerList[j] = sortedPlayerList[j - 1];
+                sortedPlayerList[j - 1] = tampungan;
+                j = j - 1;
+            }
+        }
+        return sortedPlayerList;
     }
 }
